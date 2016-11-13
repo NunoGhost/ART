654fe43d4a15f60e05d455f8dc632ca6984fa58d
NunoGhost <up201502854@fe.up.pt>
2016-11-13 22:15:07 +0000

    Models and other stuff

--- Assets/Assets/Scripts/CurlingStoneScript.cs
+++ Assets/Assets/Scripts/CurlingStoneScript.cs
@@ -4,20 +4,25 @@ using System.Collections;
 public class CurlingStoneScript : MonoBehaviour {
 
 	private Rigidbody rigidBody;
+	private GameObject buttonMarker;
+	public Vector3 velocity;
 
 	public float speed;
+	public float friction;
 	public bool fire = false;
 	public bool stopped = false;
 
 	// Use this for initialization
 	void Awake () {
 		rigidBody = GetComponent<Rigidbody> ();
+		buttonMarker = GameObject.Find ("ButtonMarker");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKey(KeyCode.Space)){
 			fire = true;	
+			rigidBody.AddRelativeForce( new Vector3 (0, speed, 0));
 		}
 	}
 
@@ -25,10 +30,15 @@ public class CurlingStoneScript : MonoBehaviour {
 		if (fire) {
 			Move ();
 		}
+
+		if (!fire) {
+			gameObject.transform.localEulerAngles = new Vector3 (0, 0, buttonMarker.transform.localEulerAngles.z);
+		}
 	}
 
 	void Move(){
-		rigidBody.velocity = new Vector3 (0, 0, speed);
+		velocity = rigidBody.velocity;
+		rigidBody.AddRelativeForce (new Vector3 (0, -friction, 0));
 		speed -= 0.00001f;
 		if (speed <= 0) {
 			fire = false;
